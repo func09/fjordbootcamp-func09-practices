922581c0dc2d4b0e6da9aff865d7c43e80de3733
Mitsuru Haga <mitsuru.haga@gmail.com>
2022-02-19 14:57:34 +0900

    配列をソートする処理を追加しました

--- practices/48/src/sort.js
+++ practices/48/src/sort.js
@@ -1,2 +1,12 @@
+var sortNumber = function (number) {
+  number.sort(function (a, b) {
+    if (a == b) {
+      return 0;
+    }
+    return a < b ? -1 : 1;
+  });
+};
+
 var number = [19, 3, 81, 1, 24, 21];
+sortNumber(number);
 console.log(number);
