36b16fd6d9e93a22c95f94889dd25150c8934700
Mitsuru Haga <mitsuru.haga@gmail.com>
2022-02-28 15:49:29 +0900

    練習問題7~10の問題内容を転記した

---
+++ practices/22/spec/lesson_10_spec.rb
@@ -0,0 +1,17 @@
+describe "練習問題10" do
+  # sinatra を使っておみくじを引くWebアプリを作ってください。
+  # 出てくるのは、大吉、中吉、末吉、凶とします。
+  describe "問1" do
+    example
+  end
+
+  # P.244で書いた sinatra_hi.rb へHTTPアクセスするプログラムを書いてください。
+  describe "問2" do
+    example
+  end
+
+  # P.246で書いた sinatra_drink.rb へHTTPアクセスするプログラムを書いてください。
+  describe "問2" do
+    example
+  end
+end
